9c5504e00a2297a35207ef6f7fee92b648532078
VictorWest <victorwaribokowest@gmail.com>
2023-10-18 15:21:13 +0100

    exam countdown

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var deadLine = (new Date().getTime() + 1209600000);
+var deadLine = new Date("dec 4, 2023, 8:00:00").getTime();
 
 var x = setInterval(function(){
     var currentDate = new Date().getTime();
@@ -6,7 +6,7 @@ var x = setInterval(function(){
 
     var days = Math.floor(d / (1000 * 60 * 60 * 24));
     console.log(days);
-    document.querySelector(".firstRow div").innerHTML = `${days}`;
+    document.querySelector(".firstRow").innerHTML = `${days}`;
     var hours = Math.floor((d % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     console.log(hours);
     document.querySelector(".secondRow").innerHTML = `${hours}`;
@@ -16,6 +16,20 @@ var x = setInterval(function(){
     var seconds = Math.floor((d % (1000 * 60)) / (1000));
     console.log(seconds);
     document.querySelector(".fourthRow").innerHTML = `${seconds}`;
+
+    if(d < 0){
+        clearInterval(x);
+        document.getElementById("heading").innerHTML = `SUCCESS IN YOUR EXAMS`;
+        document.querySelector(".firstRow").innerHTML = `...`;
+        document.querySelector(".secondRow").innerHTML = `...`;
+        document.querySelector(".thirdRow").innerHTML = `...`;
+        document.querySelector(".fourthRow").innerHTML = `...`;
+        for(var i = 0; i < (document.querySelectorAll("p")).length; i++){
+            document.querySelectorAll("p")[i].innerHTML = "";
+            console.log((document.querySelectorAll("p")).length);
+        }
+        
+    }
 }, 1000);
 
 
